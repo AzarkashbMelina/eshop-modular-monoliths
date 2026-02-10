798b2d71516f4d91c292527ad63f4551e9a47c0b
m.azargashb <m.azargashb@ECOM-PC36>
2026-02-10 16:22:30 +0330

    add catalog dbcontext and add ef core packages

---
+++ Modules/Catalog/Catalog/Data/CatalogDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.Data;
+
+public class CatalogDbContext : DbContext
+{
+    public CatalogDbContext(DbContextOptions<CatalogDbContext> options) 
+        : base(options) { }
+
+    public DbSet<Product> Products => Set<Product>(); //this will represent product table on the postgres
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("catalog"); //for data isolation and schema separation
+        builder.Entity<Product>().Property(c => c.Name).IsRequired().HasMaxLength(100); 
+        base.OnModelCreating(builder);
+    }
+}
